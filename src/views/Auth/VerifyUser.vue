6e98e116c901ffea936493ab307bd31526ae6472
Hugo Richard <hrichard206@gmail.com>
2022-12-07 17:03:12 +0100

    Change toast on verify

--- src/views/Auth/VerifyUser.vue
+++ src/views/Auth/VerifyUser.vue
@@ -5,16 +5,21 @@
         <h1 class="title">{{ $t("verifyEmail") }}</h1>
         <p class="text">{{ $t("verifyEmailText") }}</p>
       </div>
-      <div class="form-item">
-        <label class="label" for="verify">{{ $t("verify") }}</label>
-        <input class="input mt-sm" type="text" id="verify" placeholder="1262143172" v-model="code"/>
+      <div class="verify-form" v-if="!isLoading">
+        <div class="form-item">
+          <label class="label" for="verify">{{ $t("verify") }}</label>
+          <input class="input mt-sm" type="text" id="verify" placeholder="1262143172" v-model="code"/>
+        </div>
+        <div class="form-item center" @click="verify">
+          <button class="btn-primary fullwidth">{{ $t("verify") }}</button>
+        </div>
+        <div class="verify-footer center">
+          <p class="text">{{ $t("verifyEmailNotReceived") }}</p>
+          <button class="btn-secondary mt-md" @click="getVerificationCode">{{ $t("resendCode") }}</button>
+        </div>
       </div>
-      <div class="form-item center" @click="verify">
-        <button class="btn-primary fullwidth">{{ $t("verify") }}</button>
-      </div>
-      <div class="verify-footer center">
-        <p class="text">{{ $t("verifyEmailNotReceived") }}</p>
-        <button class="btn-secondary mt-md" @click="getVerificationCode">{{ $t("resendCode") }}</button>
+      <div class="center" v-else>
+        <i class="fa-solid fa-circle-notch fa-spin"></i>
       </div>
     </div>
   </div>
@@ -31,6 +36,11 @@ export default {
       code: "",
     };
   },
+  computed: {
+    isLoading() {
+      return this.$store.getters.isLoading;
+    },
+  },
   methods: {
     verify() {
       this.$store.dispatch("loading", true);
@@ -45,27 +55,34 @@ export default {
             this.$store.dispatch("loading", false);
             this.$store.dispatch("insertUser", res.data.verifyUser);
             this.$router.push({name: "Home"});
+            this.$swal({
+              title: this.$t("verifyEmailSuccess"),
+              toast: true,
+              position: "top-end",
+              icon: "success",
+              timer: 1500,
+              timerProgressBar: true,
+              showConfirmButton: false
+            });
           } else {
             this.$store.dispatch("loading", false);
-            this.$swal(this.$t("verifyError"));
+            this.$swal({
+              title: this.$t("verifyEmailError"),
+              toast: true,
+              position: "top-end",
+              icon: "error",
+              timer: 1500,
+              timerProgressBar: true,
+              showConfirmButton: false
+            });
           }
         })
         .catch((err) => {
           console.log(err);
           this.$store.dispatch("loading", false);
-          this.$store.dispatch("toast", {
-            title: this.$t("verifyError"),
-            toast: true,
-            icon: "error",
-            timer: 1500,
-            position: "top-end",
-            timerProgressBar: true,
-            showConfirmButton: false,
-          });
         });
     },
     getVerificationCode() {
-      this.$store.dispatch("loading", true);
       const user = this.$store.getters.user;
       this.$apollo.mutate({
         mutation: getVerificationCode,
@@ -74,12 +91,26 @@ export default {
         },
       })
         .then(() => {
-          this.$store.dispatch("loading", false);
-          this.$swal(this.$t("verifyCodeSuccess"), "", "success");
+          this.$swal({
+            title: this.$t("CodeSent"),
+            toast: true,
+            position: "top-end",
+            icon: "success",
+            timer: 1500,
+            timerProgressBar: true,
+            showConfirmButton: false
+          });
         })
         .catch(() => {
-          this.$store.dispatch("loading", false);
-          this.$swal(this.$t("verifyCodeError"), "", "error");
+          this.$swal({
+            title: this.$t("CodeSentError"),
+            toast: true,
+            position: "top-end",
+            icon: "error",
+            timer: 1500,
+            timerProgressBar: true,
+            showConfirmButton: false
+          });
         });
     },
   },
