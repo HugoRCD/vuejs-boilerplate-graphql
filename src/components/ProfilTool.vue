d949a8f841bfca7c9689181bedb6632a65253354
Hugo Richard <hrichard206@gmail.com>
2022-12-08 13:14:59 +0100

    Switch textSupport to component

--- src/components/ProfilTool.vue
+++ src/components/ProfilTool.vue
@@ -13,9 +13,7 @@
           {{ $t(link.name.toLowerCase()) }}
         </router-link>
         <div class="separator"></div>
-        <div class="dropdown-profil-item" @click="textSupport">
-          {{ $t('support') }}
-        </div>
+        <TextSupport/>
         <div class="separator"></div>
         <div class="dropdown-profil-item logout" @click="logout()">
           {{ $t("logout") }}
@@ -26,8 +24,11 @@
 </template>
 
 <script>
+import TextSupport from "@/components/TextSupport.vue";
+
 export default {
   name: "ProfilTool",
+  components: {TextSupport},
   data() {
     return {
       menuOpen: false,
@@ -90,48 +91,6 @@ export default {
             icon: "success",
           });
           this.menuOpen = false;
-        }
-      });
-    },
-    textSupport() {
-      this.$swal({
-        title: this.$t("support"),
-        html: `
-          <div class="form">
-            <div class="form-item left">
-              <p class="text">${this.$t("supportText")}</p>
-            </div>
-            <div class="form-item left">
-              <label class="label" for="subject">${this.$t("subject")}</label>
-              <input class="input mt-sm" id="subject" class="swal2-input" placeholder="Bug, informations...">
-            </div>
-            <div class="form-item left">
-              <label class="label" for="message">${this.$t("message")}</label>
-              <textarea class="input mt-sm" id="message" class="swal2-textarea" placeholder="${this.$t("message")}"></textarea>
-            </div>
-          </div>
-        `,
-        toast: false,
-        focusConfirm: false,
-        timer: false,
-        position: "middle",
-        showCancelButton: true,
-        showConfirmButton: true,
-        confirmButtonText: this.$t("send"),
-        cancelButtonText: this.$t("cancel"),
-        background: "var(--bg-primary)",
-        color: "var(--font-color)",
-        preConfirm: () => {
-          return {
-            subject: document.getElementById("subject").value,
-            message: document.getElementById("message").value
-          };
-        }
-      }).then(result => {
-        if (result.isConfirmed) {
-          this.$swal({
-            text: this.$t("supportSuccess"),
-          });
         }
       });
     }
